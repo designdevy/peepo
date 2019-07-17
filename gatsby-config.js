a743bfa11d9e9ca9c3f453c796ee6b53449360d9
Muhammad Athar <muhammad.athar@outlook.co.th>
2019-07-17 23:42:12 +0700

    added typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,8 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    name: `Muhammadathar.com`,
+  },
+  plugins: [`gatsby-plugin-typescript`, "gatsby-plugin-tslint"],
 }
