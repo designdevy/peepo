8051b005707d9f47e578e9f016c0ef0e7b28b2ea
Muhammad Athar <muhammad.athar@agoda.com>
2019-07-26 01:27:33 +0700

    added title metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@
 module.exports = {
   siteMetadata: {
     name: `Muhammadathar.com`,
+    title: `Muhammad Athar`,
+    description: `Personal Website`,
+    siteUrl: `https://www.muhammadathar.com`,
   },
-  plugins: [`gatsby-plugin-typescript`, "gatsby-plugin-tslint", `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    "gatsby-plugin-tslint",
+    `gatsby-plugin-styled-components`,
+  ],
 }
